1a1f0078c17f3ca622869b64d42fdf0406ea4590
jaa <jaa@sn127.fi>
2016-12-18 16:34:08 +0200

    build: bump sbt to 0.13.13 and remove dead scalafx

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,6 @@ object Dependencies {
   val configVersion = "1.3.1"
   val scalaXMLVersion = "1.0.6"
   val scalaParserCombinatorsVersion = "1.0.4"
-  val scalaFXVersion = "8.0.102-R11"
 
   // Libraries
   val scalatest = "org.scalatest" %% "scalatest" % scalatestVersion
@@ -18,5 +17,4 @@ object Dependencies {
   val config = "com.typesafe" % "config" % configVersion
   val scalaXML = "org.scala-lang.modules" %% "scala-xml" % scalaXMLVersion
   val scalaParserCombinators = "org.scala-lang.modules" %% "scala-parser-combinators" % scalaParserCombinatorsVersion
-  val scalaFX = "org.scalafx" %% "scalafx" % scalaFXVersion
 }
